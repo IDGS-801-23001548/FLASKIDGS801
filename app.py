bbac2751de87ee687a5d42ae455ccb55a205f240
IDGS-801-23001548 <84168@alumnos.utleon.edu.mx>
2026-02-16 19:04:38 -0600

    Insert 16/02/2026

--- app.py
+++ app.py
@@ -23,13 +23,13 @@ def index():
     alumno = Alumnos.query.all()
     return render_template("index.html", form=create_form, alumno=alumno)
 
-@app.route('/Alumnos', methods=['GET','POST'])
-def Alumnos():
+@app.route('/alumnos', methods=['GET','POST'])
+def alumnos():
     create_form = forms.UserForm(request.form)
     if request.method == 'POST':
         alum = Alumnos(nombre = create_form.nombre.data,
                        apaterno = create_form.apaterno.data,
-                       email = create_form.email.data)
+                       email = create_form.correo.data)
         db.session.add(alum)
         db.session.commit()
         return redirect(url_for('index'))
